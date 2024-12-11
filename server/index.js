6d19c5d7816ae27635857544d6b69e9de131f056
22piyush <piyushaglawe05@gmail.com>
2024-12-11 21:33:15 +0530

    done

--- server/index.js
+++ server/index.js
@@ -8,19 +8,26 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get = ("/health",(req, res) => {
-    res.jsonres.json(
-        { 
-            message: "Server is up and Running..." 
-        });
-  });
-
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI);
   if (conn) {
     console.log("connected to mongodb");
   }
 };
+
+app.get = ("/health",(req, res) => {
+    res.json({
+      success: true,
+      message: "Server is up and Running...",
+    });
+});
+
+app.use("*",(req,res)=>{
+  res.status(404).json({
+    success:false,
+    message:"Route Not Found"
+  })
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
