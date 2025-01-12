2199324bd7a06e0d707b4c7b646e9f4f974ba1f5
22piyush <piyushaglawe05@gmail.com>
2025-01-12 11:58:34 +0530

    routing setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./index.css";
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from './views/Home';
+import Login from './views/Login';
+import SignUp from './views/SignUp';
+import NotFound from './views/404';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-   <h1 className='text-red-500'>E-Commerece</h1>
-);
 
+const router = createBrowserRouter([
+  {
+    path: "/home",
+    element: <Home/>, 
+  },
+  {
+   path: "/login",
+   element: <Login/>, 
+ },
+ {
+   path: "/signup",
+   element: <SignUp/>
+ },
+ {
+   path: "/*",
+   element: <NotFound/>
+ },
+]);
+
+root.render(
+  <RouterProvider router={router} />
+);
