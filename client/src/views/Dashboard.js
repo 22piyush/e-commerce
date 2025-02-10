21a96ae4aec280b466dcd04b32685724ac153a03
22piyush <piyushaglawe05@gmail.com>
2025-02-10 22:03:38 +0530

    done

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -14,6 +14,11 @@ function Dashboard() {
     const user = getCurrentUser();
     if (user) {
       setUser(user);
+    }else{
+      toast.error("Please login to access this page");
+       setTimeout(() => {
+        window.location.href = '/login';
+      }, 2000)
     }
   }, []);
 
